df38b18398a066cabd4ae3b7bae07fa554bd6b77
sixpeteunder <plenjo15@gmail.com>
2023-06-01 11:29:44 +0300

    Add library code

--- LibSample.cs
+++ LibSample.cs
@@ -1,6 +1,35 @@
-﻿namespace LibSample;
+﻿using System.Runtime.InteropServices;
+
+namespace LibSample;
 
 public static class LibSample
 {
+    const string GREETING = "Hello there";
 
+    [UnmanagedCallersOnly(EntryPoint = "add")]
+    public static int Add(int a, int b) => a + b;
+
+    [UnmanagedCallersOnly(EntryPoint = "subtract")]
+    public static int Subtract(int a, int b) => a - b;
+
+    [UnmanagedCallersOnly(EntryPoint = "multiply")]
+    public static int Multiply(int a, int b) => a * b;
+
+    // UnmanagedCallersOnly methods only accept primitive arguments. 
+    // The primitive arguments have to be marshalled manually if necessary.
+    [UnmanagedCallersOnly(EntryPoint = "greet")]
+    public static IntPtr Greet(IntPtr namePointer) 
+    {
+        // Parse string from the passed pointer
+        string name = Marshal.PtrToStringAnsi(namePointer) ?? "friend";
+
+        // Concatenate strings 
+        string greeting = $"{GREETING}, {name}";
+
+        // Assign pointer of the concatenated string to sumPointer
+        IntPtr sumPointer = Marshal.StringToHGlobalAnsi(greeting);
+
+        // Return pointer
+        return sumPointer;
+    }
 }
